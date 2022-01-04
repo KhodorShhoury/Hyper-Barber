754e205773ea197cea8b98a9222450f2f00db83b
KhodorShhoury <khodorshhoury1@gmail.com>
2022-01-04 17:13:40 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//get landing
+let landing = document.querySelector(".landing");
+
+let landingImgs= ["01.jpg","02.jpg","03.jpg","04.jpg"];
+
+setInterval(()=>{
+let landingImg=Math.floor(Math.random() * landingImgs.length);
+
+landing.style.backgroundImage=`url(/imgs/${landingImgs[landingImg]})`;
+
+},4000);
+
+// services
+let services = document.querySelector(".services");
+//get services cards
+let servicesCards= Array.from(document.querySelectorAll(".services .card")) ;
+
